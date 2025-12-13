d8316f33e97a37097d9e67a5471e3191f16fddc6
huizhenkok <kokhuizhen@gmail.com>
2025-12-13 15:18:04 +0800

    build and making css to landing page and login

--- frontend/src/app/components/landing/landing.ts
+++ frontend/src/app/components/landing/landing.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // 👈 只需要引入这个
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // 👈 记得注册
   templateUrl: './landing.html',
-  styleUrl: './landing.css',
+  styleUrl: './landing.css'
 })
 export class Landing {
-
+  // 首页不需要什么逻辑代码，空的就行
 }
--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // 处理表单输入
+import { Router, RouterLink } from '@angular/router'; // 处理跳转
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
 
+  loginObj: any = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    // 模拟登录逻辑
+    // 之后我们会在这里连接 Backend API
+    if (this.loginObj.email && this.loginObj.password) {
+      // 成功登录
+      alert("登录成功！欢迎来到 G5ConfEase");
+      this.router.navigateByUrl('/dashboard');
+    } else {
+      // 失败
+      alert("请输入邮箱和密码");
+    }
+  }
 }
