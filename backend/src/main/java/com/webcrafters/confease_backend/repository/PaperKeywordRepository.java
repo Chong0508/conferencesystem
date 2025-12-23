da4401859069eabb4b4b0204d54c57a2ffa06995
munkei <mk.chong5@gmail.com>
2025-12-23 22:13:48 +0800

    Add edit & delete function for my submissions

--- backend/src/main/java/com/webcrafters/confease_backend/repository/PaperKeywordRepository.java
+++ backend/src/main/java/com/webcrafters/confease_backend/repository/PaperKeywordRepository.java
@@ -2,9 +2,19 @@ package com.webcrafters.confease_backend.repository;
 
 import com.webcrafters.confease_backend.model.PaperKeyword;
 import com.webcrafters.confease_backend.model.PaperKeywordId;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaperKeywordRepository extends JpaRepository<PaperKeyword, PaperKeywordId> {
+  @Modifying
+    @Transactional
+    @Query("DELETE FROM PaperKeyword pk WHERE pk.paper_id = :paperId")
+    void deleteByPaperId(@Param("paperId") Long paperId);
 }
