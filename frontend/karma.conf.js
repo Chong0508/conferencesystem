f076e4561c0d4ff20ab4a5e1753095e2f3266cec
munkei <mk.chong5@gmail.com>
2025-12-26 13:35:39 +0800

    Update

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,58 +1,30 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-allure-reporter') // --- ADDED THIS ---
+      require('karma-allure-reporter') // Ensure this is present
     ],
-    client: {
-      jasmine: {
-        // Configuration options for Jasmine
-      },
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true 
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-
-    // --- ADDED ALLURE REPORTER CONFIG ---
-    reporters: ['progress', 'kjhtml', 'allure'], 
+    reporters: ['progress', 'kjhtml', 'allure'],
     allureReporter: {
-      reportDir: 'allure-results', // This generates results in frontend/allure-results
+      // Use an absolute path to prevent the 'undefined path' error
+      reportDir: path.join(__dirname, 'allure-results'),
       useTimestamp: false
     },
-    // ------------------------------------
-    
-    // --- UPDATED FOR DOCKER/JENKINS ---
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
-    // ----------------------------------
-
-    restartOnFileChange: true,
-    singleRun: true // Ensures the process exits after tests finish in CI
+    singleRun: true,
+    restartOnFileChange: true
   });
 };
--- frontend/src/test-setup.ts
+++
@@ -1,6 +0,0 @@
-import { JasmineAllureReporter } from 'jasmine-allure2-reporter';
-
-// This registers the reporter globally for all Jasmine tests
-jasmine.getEnv().addReporter(new JasmineAllureReporter({
-  allureReportDir: 'allure-results' 
-} as any));
