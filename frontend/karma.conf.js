f8d9521186f0979de45b4a1a414ca3ba467d3987
munkei <mk.chong5@gmail.com>
2025-12-26 13:47:57 +0800

    Update

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -4,26 +4,43 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-allure-reporter') // <--- Make sure this is installed
+      require('karma-allure-reporter')
     ],
+
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+
     reporters: ['progress', 'kjhtml', 'allure'],
+
     allureReporter: {
-      reportDir: path.join(__dirname, 'allure-results'),
+      // ✅ CORRECT KEY NAME
+      outputDir: path.join(__dirname, 'allure-results'),
       useTimestamp: false
     },
+
     browsers: ['ChromeHeadlessNoSandbox'],
+
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
       }
     },
+
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: false
   });
 };
