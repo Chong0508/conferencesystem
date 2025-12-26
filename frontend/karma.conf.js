bfc67055cdc0cb401446c274d5927688d5ef9cc1
munkei <mk.chong5@gmail.com>
2025-12-26 14:39:16 +0800

    Update

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -3,31 +3,33 @@ const path = require('path');
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'], // <-- Remove angularKarma.framework
-
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-allure-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma') // <-- just load the plugin
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-
     client: {
-      jasmine: {},
-      clearContext: false
+      jasmine: {
+        // configuration options for Jasmine
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    reporters: ['progress', 'kjhtml', 'allure'],
-
-    allureReporter: {
-      outputDir: path.join(__dirname, 'allure-results'),
-      useTimestamp: false
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadlessNoSandbox'],
-
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
@@ -39,7 +41,6 @@ module.exports = function (config) {
         ]
       }
     },
-
     singleRun: true,
     restartOnFileChange: false
   });
