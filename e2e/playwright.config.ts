5e205efe4dad062744344c818389d9753b11d77c
munkei <mk.chong5@gmail.com>
2025-12-26 09:59:25 +0800

    Add E2E TEST

---
+++ e2e/playwright.config.ts
@@ -0,0 +1,21 @@
+// Ensure Playwright knows Angular run at localhost:4200
+
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:4200',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
