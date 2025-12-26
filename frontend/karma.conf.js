5e4fbdb72ce1589ddc7668f994a91dd7941217b8
munkei <mk.chong5@gmail.com>
2025-12-26 11:46:45 +0800

    Add karma.config & fix jenkins

---
+++ frontend/karma.conf.js
@@ -0,0 +1,50 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {
+        // Configuration options for Jasmine
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true 
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    
+    // --- UPDATED FOR DOCKER/JENKINS ---
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    },
+    // ----------------------------------
+
+    restartOnFileChange: true,
+    singleRun: true // Ensures the process exits after tests finish in CI
+  });
+};
